397f89089b8a0eb30a39556e4d2952c62abcd633
Benjamin Alterman <balterman94@gmail.com>
2019-11-03 22:23:36 -0600

    update

--- Code/MyNewHome/routes.py
+++ Code/MyNewHome/routes.py
@@ -1,16 +1,7 @@
-from datetime import datetime
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
-
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-
-from models import User, Post
+from flask import render_template, url_for, flash, redirect
+from MyNewHome import app
+from MyNewHome.forms import RegistrationForm, LoginForm
+from MyNewHome.models import User, Post
 
 
 posts = [
@@ -65,7 +56,3 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
