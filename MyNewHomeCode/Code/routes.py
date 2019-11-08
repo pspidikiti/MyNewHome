dc28496f15f91d06b62f2e3cc2a24922734a8141
Benjamin Alterman <balterman94@gmail.com>
2019-11-07 23:00:32 -0600

    update

--- MyNewHomeCode/Code/routes.py
+++ MyNewHomeCode/Code/routes.py
@@ -102,8 +102,7 @@ def new_post():
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post',
-                           form=form, legend='New Post')
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
 
 
 @app.route("/post/<int:post_id>")
@@ -128,8 +127,7 @@ def update_post(post_id):
     elif request.method == 'GET':
         form.title.data = post.title
         form.content.data = post.content
-    return render_template('create_post.html', title='Update Post',
-                           form=form, legend='Update Post')
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
 
 
 @app.route("/post/<int:post_id>/delete", methods=['POST'])
