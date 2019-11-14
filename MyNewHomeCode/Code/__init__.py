991d3fc2c9df16d992ea5ac9fe12be305013799b
Benjamin Alterman <balterman94@gmail.com>
2019-11-13 23:54:29 -0600

    update

--- MyNewHomeCode/Code/__init__.py
+++ MyNewHomeCode/Code/__init__.py
@@ -17,7 +17,7 @@ app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASSWORD')
 mail = Mail(app)
 
 from Code import routes
